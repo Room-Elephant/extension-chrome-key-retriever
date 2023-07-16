c7d9b666d2103cef6314a47d84176a978aab3585
JPedroBorges <JPedroBorges@users.noreply.github.com>
2023-07-16 02:34:38 +0100

    fix unformatted files

--- src/handler/store.js
+++ src/handler/store.js
@@ -1,58 +1,57 @@
 function appStore(onStoreUpdate) {
-    let storeItems = [];
-  
-    chrome.storage.onChanged.addListener((changes, namespace) => {
-      if (namespace !== "local") return;
-      if (!changes?.storeItems) return;
-  
-      storeItems = changes.storeItems.newValue;
-      onStoreUpdate(storeItems);
-    });
-  
-    async function getItems() {
-      try {
-        const storeResult = await chrome.storage.local.get(["storeItems"]);
-        return storeResult["storeItems"];
-      } catch (e) {
-        console.log("🐶 ~ could not get items from extension store:", e);
-      }
+  let storeItems = [];
+
+  chrome.storage.onChanged.addListener((changes, namespace) => {
+    if (namespace !== "local") return;
+    if (!changes?.storeItems) return;
+
+    storeItems = changes.storeItems.newValue;
+    onStoreUpdate(storeItems);
+  });
+
+  async function getItems() {
+    try {
+      const storeResult = await chrome.storage.local.get(["storeItems"]);
+      return storeResult["storeItems"];
+    } catch (e) {
+      console.log("🐶 ~ could not get items from extension store:", e);
     }
-  
-    async function addItem(newItem) {
-      try {
-        let lastId = getLastId();
-        newItem.id = ++lastId;
-  
-        storeItems.push(newItem);
-  
-        return new Promise((resolve) => {
-          chrome.storage.local.set({ storeItems }, function () {
-            resolve();
-          });
-        });
-      } catch (e) {
-        console.log("🐶 ~ could not add items to extension store:", e);
-      }
-    }
-  
-    async function removeItem(itemId) {
-      try {
-        await chrome.storage.local.set({
-          storeItems: storeItems.filter(({ id }) => !(itemId === id)),
-        });
-      } catch (e) {
-        console.log("🐶 ~ could not delete items from extension store:", e);
-      }
-    }
-  
-    function getLastId() {
-      return [...storeItems].sort((a, b) => a.id - b.id).slice(-1)[0]?.id || 0;
-    }
-  
-    return {
-      addItem,
-      removeItem,
-      getItems,
-    };
   }
-  
+
+  async function addItem(newItem) {
+    try {
+      let lastId = getLastId();
+      newItem.id = ++lastId;
+
+      storeItems.push(newItem);
+
+      return new Promise((resolve) => {
+        chrome.storage.local.set({ storeItems }, function () {
+          resolve();
+        });
+      });
+    } catch (e) {
+      console.log("🐶 ~ could not add items to extension store:", e);
+    }
+  }
+
+  async function removeItem(itemId) {
+    try {
+      await chrome.storage.local.set({
+        storeItems: storeItems.filter(({ id }) => !(itemId === id)),
+      });
+    } catch (e) {
+      console.log("🐶 ~ could not delete items from extension store:", e);
+    }
+  }
+
+  function getLastId() {
+    return [...storeItems].sort((a, b) => a.id - b.id).slice(-1)[0]?.id || 0;
+  }
+
+  return {
+    addItem,
+    removeItem,
+    getItems,
+  };
+}
--- src/page.js
+++ src/page.js
@@ -28,7 +28,9 @@ function appPage(storeSave, storeDelete, storeSet) {
   };
   let presentationItems = [];
 
-  document.getElementById("addKeyBtn").addEventListener("click", () => show(PAGES.ADD));
+  document
+    .getElementById("addKeyBtn")
+    .addEventListener("click", () => show(PAGES.ADD));
 
   document.getElementById("saveKeyBtn").addEventListener("click", () => {
     if (formValidation()) onSaveItem();
@@ -77,11 +79,16 @@ function appPage(storeSave, storeDelete, storeSet) {
     if (page.keyListElement) keyListElement.classList.remove("display-none");
     else keyListElement.classList.add("display-none");
 
-    if (page.keyListFooterElement) keyListFooterElement.classList.remove("display-none");
+    if (page.keyListFooterElement)
+      keyListFooterElement.classList.remove("display-none");
     else keyListFooterElement.classList.add("display-none");
 
-    if (page.listActions) [...listActions].forEach((action) => action.classList.remove("invisible"));
-    else[...listActions].forEach((action) => action.classList.add("invisible"));
+    if (page.listActions)
+      [...listActions].forEach((action) =>
+        action.classList.remove("invisible")
+      );
+    else
+      [...listActions].forEach((action) => action.classList.add("invisible"));
 
     [...viewButtons].forEach((btn) => {
       if (btn.lastChild.classList.contains("fa-eye-slash")) {
@@ -134,7 +141,9 @@ function appPage(storeSave, storeDelete, storeSet) {
   }
 
   function onDeleteItem(itemId) {
-    const idToRemove = presentationItems.find((element) => itemId === element.id).id;
+    const idToRemove = presentationItems.find(
+      (element) => itemId === element.id
+    ).id;
 
     storeDelete(idToRemove);
   }
@@ -164,7 +173,7 @@ function appPage(storeSave, storeDelete, storeSet) {
       },
       function (err) {
         console.log("🐶 ~ could not copy text due to:", err);
-      },
+      }
     );
   }
 
@@ -192,7 +201,9 @@ function appPage(storeSave, storeDelete, storeSet) {
     const alias = document.getElementById("alias").value;
     const key = document.getElementById("key").value;
     const subKey = document.getElementById("subKey").value || undefined;
-    const storageType = document.querySelector('input[name="storage"]:checked').value;
+    const storageType = document.querySelector(
+      'input[name="storage"]:checked'
+    ).value;
 
     return { alias, key, subKey, type: storageType };
   }
