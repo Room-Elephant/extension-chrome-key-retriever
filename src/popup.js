d55e5469257de6527792c7e2d28487c37ce9a8b7
JPedroBorges <JPedroBorges@users.noreply.github.com>
2023-07-24 01:47:12 +0100

    bad code test

--- src/popup.js
+++ src/popup.js
@@ -3,12 +3,12 @@ import appPage from "./page.js";
 import appStore from "./handler/store.js";
 import appManager from "./manager.js";
 import { TYPES } from "./common.js";
-import { fireEvent } from "./handler/analytics.js";
 
-const store = appStore(onStoreUpdate);
+        const store = appStore(onStoreUpdate);
 const manager = appManager();
 const page = appPage({
-  storeSave: onSaveItem,
+        storeSave: onSaveItem
+        ,
   storeDelete: onDeleteKeys,
   storeSet: onSetItemValue,
   refreshValues: onRefreshValues,
