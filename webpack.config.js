ada9253305c01e261498e40678266c28dccfec80
JPedroBorges <JPedroBorges@users.noreply.github.com>
2023-07-25 18:32:04 +0100

    Update webpack.config.js
    Co-authored-by: Mariana Rodrigues <mariana.cf.rodrigues.95@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /.s?css$/,
+        test: /.css$/,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
